813c343891342f6e80ee8d53ed8eba2987dda6c3
benakram4 <105305419+benakram4@users.noreply.github.com>
2022-12-02 01:10:51 -0500

    no date is history + double links after log in

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 /************************************************************************* 
- *  BTI325– Assignment 5
+ *  BTI325– Assignment 6
  *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.
  *   No part of this assignment has been copied manually or electronically from any other source. *
  * (including 3rd party web sites) or distributed to other students. *  
- *  Name: Ben Akram Student ID: 158523217 Date: 27/11/2022 *
- *  Online (Cyclic) URL:  * https://friendly-eel-pumps.cyclic.app *
+ *  Name: Ben Akram Student ID: 158523217 Date: xx/xx/xxxx *
+ *  Online (Cyclic) URL:  *  *
  * ********************************************************************************/
 
 // ======Create express application ================================================================
@@ -14,6 +14,33 @@ app.use(express.static('public')); // serve static files from the public folder
 app.use(express.json()); // parse incoming JSON requests and place them in req.body
 app.use(express.urlencoded({ extended: true })); // parse incoming urlencoded form data and place it in req.body
 // =================================================================================================
+
+// === mongoose & cookie setup  ====================================================================
+var dataServiceAuth = require("./data-service-auth.js");
+var clientSessions = require("client-sessions");
+
+app.use(clientSessions({
+    cookieName: "session", // this is the object name that will be added to 'req'
+    secret: "web322_assignment6_is_the_best", // this should be a long un-guessable string.
+    duration: 2 * 60 * 1000, // duration of the session in milliseconds (2 minutes)
+    activeDuration: 1000 * 60 // the session will be extended by this many ms each request (1 minute)
+}));
+app.use(function(req,res,next){
+    res.locals.session = req.session;
+    next();
+});
+
+ensureLogin = function (req, res, next) {
+    if (!req.session.user) {
+        res.redirect("/login");
+    } else {
+        next();
+    }
+}
+
+
+// =================================================================================================
+
 
 // === Create path to static files =================================================================
 var path = require("path"); // include module path to use __dirname, and function path.join()
@@ -102,20 +129,20 @@ app.get("/about", function(req, res){
 });
 
 //employees/add route
-app.get("/employees/add", function(req, res){
+app.get("/employees/add", ensureLogin, function(req, res){
     data.getDepartments()
     .then((data) => res.render("addEmployee", {departments: data}))
     .catch((err) => res.render("addEmployee", {departments: []}));
 });
 
 //images/add route
-app.get("/images/add", function(req, res){
+app.get("/images/add", ensureLogin,function(req, res){
     //redirect to images page if render was successful
     res.render("addImage");
 });
 
 // images route
-app.get("/images", function(req, res){
+app.get("/images", ensureLogin,function(req, res){
     //return all images in the images folder
     fs.readdir("./public/images/uploaded", (err, images) =>{
         if(err){
@@ -129,9 +156,8 @@ app.get("/images", function(req, res){
 });
 
 
-
 //employees route and employee related queries
-app.get("/employees", function(req, res){
+app.get("/employees", ensureLogin,function(req, res){
 
     if(req.query.status){ //query with employees status
         console.log("this is req.query.status: " + req.query.status);
@@ -161,7 +187,7 @@ app.get("/employees", function(req, res){
 });
 
 //managers route
-app.get("/managers", function(req, res){
+app.get("/managers", ensureLogin,function(req, res){
     data.getManagers()
     .then((data) => {res.json(data)})
     .catch((err) => {console.log(`${err}`)});
@@ -169,7 +195,7 @@ app.get("/managers", function(req, res){
 });
 
 //get employee by emp number function
-app.get("/employee/:empNum", (req, res) => {
+app.get("/employee/:empNum", ensureLogin,(req, res) => {
 	// initialize an empty object to store the valueslet 
 	viewData = {};
 
@@ -215,7 +241,7 @@ app.get("/employee/:empNum", (req, res) => {
 
 
 //delete employee by emp number function
-app.get("/employees/delete/:empNum", (req, res) => {
+app.get("/employees/delete/:empNum", ensureLogin,(req, res) => {
     console.log('\n' + "delete emp num: " + req.params.empNum + '\n');
     data.deleteEmployeeByNum(req.params.empNum)
     .then(() => res.redirect("/employees"))
@@ -224,7 +250,7 @@ app.get("/employees/delete/:empNum", (req, res) => {
 
 
 //departments route
-app.get("/departments", function(req, res){
+app.get("/departments", ensureLogin,function(req, res){
     data.getDepartments()
     .then((data) => {res.render("departments", {departments: data})})
     .catch((err) => {res.render("departments", {message: `error${err}`})})
@@ -232,12 +258,12 @@ app.get("/departments", function(req, res){
 });
   
 //add department route
-app.get("/departments/add", function(req, res){
+app.get("/departments/add", ensureLogin,function(req, res){
     res.render("addDepartment");
 });
 
 
-app.get("/department/:value", function(req, res){
+app.get("/department/:value", ensureLogin,function(req, res){
     data.getDepartmentById(req.params.value)
     .then((data) => {res.render("department", {department: data})})
     .catch((err) => {res.status(404).send(`\nDepartment not found${err}\n`)});
@@ -248,6 +274,60 @@ app.get("/department/:value", function(req, res){
     console.log("\ngetting dep by id : getDepartmentById()\n");
 });
 
+// === GET LOGIN ROUTES ==========================================================================//
+//login route
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+//register route
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+// === POST LOGIN ROUTES ===============================================================================//
+// login post route
+app.post("/login", function(req, res){
+    req.body.userAgent = req.get('User-Agent');
+    dataServiceAuth.checkUser(req.body)
+    .then((user) => {
+        req.session.user = {
+            userName: user.userName, // authenticated user's userName
+            email: user.email, // authenticated user's email
+            loginHistory: user.loginHistory // authenticated user's loginHistory
+        }
+        res.redirect('/employees');
+    })
+    .catch((err) => {
+        res.render('login', {errorMessage: err, userName: req.body.userName});
+    });
+});
+
+// logout route
+app.get("/logout", (req, res)=>{
+    req.session.reset();
+    res.redirect("/login");
+});
+
+// user history route
+app.get("/userHistory", ensureLogin, function(req, res){
+    res.render("userHistory");
+});
+
+
+
+//register post route
+app.post("/register", function(req, res){
+    dataServiceAuth.registerUser(req.body)
+    .then(() => {
+        res.render("register", {successMessage: "User created"});
+    })
+    .catch((err) => {
+        res.render("register", {errorMessage: err, userName: req.body.userName});
+    });
+});
+
+
 //================================================================================================//
 
 //================================================================================================//
@@ -255,7 +335,7 @@ app.get("/department/:value", function(req, res){
 //================================================================================================//
 
 //upload image function
-app.post("/images/add",upload.single("imageFile") ,function(req, res){
+app.post("/images/add", ensureLogin,upload.single("imageFile") ,function(req, res){
     fs.readdir("./public/images/uploaded", (err, files) => {
         console.log("Uploading Image");
         if(err){
@@ -272,7 +352,7 @@ app.post("/images/add",upload.single("imageFile") ,function(req, res){
 });
 
 //add employee function
-app.post("/employees/add",(req,res) =>{
+app.post("/employees/add", ensureLogin,(req,res) =>{
     data.addEmployee(req.body)
     .then(() => {res.redirect("/employees")})
     .catch((err) => {res.status(500).send(`\nUnable to Add Employee: ${err} \n`)});
@@ -282,7 +362,7 @@ app.post("/employees/add",(req,res) =>{
 
 
 //update employee
-app.post("/employee/update", (req, res)=> {
+app.post("/employee/update", ensureLogin,(req, res)=> {
     console.log(req.body);
     data.updateEmployee(req.body)
     .then(() => {res.redirect("/employees")})
@@ -291,14 +371,14 @@ app.post("/employee/update", (req, res)=> {
 });
 
 //add department
-app.post("/departments/add", (req, res) => {
+app.post("/departments/add", ensureLogin,(req, res) => {
     data.addDepartment(req.body)
     .then(() => {res.redirect("/departments")})
     .catch((err) => {res.status(500).send(`\nUnable to Add Department: ${err} \n`)});
 });
 
 //update department
-app.post("/department/update", (req, res) => {
+app.post("/department/update", ensureLogin,(req, res) => {
     data.updateDepartment(req.body)
     .then(() => {res.redirect("/departments")})
     .catch((err) => {res.status(500).send(`\nUnable to Update Department: ${err} \n`)});
@@ -313,7 +393,6 @@ app.use((req, res) => {
     res.status(404).sendFile(path.join(__dirname, "views/error404.html"));
 });
   
-data.initialize().then(app.listen(HTTP_PORT, onHttpStart)).catch((err) => {
+data.initialize().then(dataServiceAuth.initialize).then(app.listen(HTTP_PORT, onHttpStart)).catch((err) => {
     console.log("Error: " + err);
 });
-//app.listen(HTTP_PORT, onHttpStart());
